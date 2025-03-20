e524c3dff24dacfd13db722db76e097d1be6ae8a
Leonardo Ferreira <leoferreira04@outlook.com>
2025-03-19 22:33:05 -0300

    Avanço na tradução (arquivo po)

--- adventure.py
+++ adventure.py
@@ -9,7 +9,7 @@ from events import death_check, death_messages, money_get, input_error
 localedir = './locales'
 appname = 'pythonAdventure'
 
-translate = gettext.translation(appname, localedir, fallback=False, languages=['en'])
+translate = gettext.translation(appname, localedir, fallback=True, languages=['en'])
 
 translate.install()
 _ = translate.gettext
